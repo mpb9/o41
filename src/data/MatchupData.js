2417483de90a2246d982cce2c66da82856a770c8
mpb9 <michaelbeebe1031@gmail.com>
2025-09-06 12:08:24 -0400

    Refactor matchup data: update team pairings and standardize quotes in MatchupData.js

--- src/data/MatchupData.js
+++ src/data/MatchupData.js
@@ -1,7 +1,7 @@
-import { USERS } from './UserData';
+import { USERS } from "./UserData";
 
 export const MATCHUPS = [
-  { teams: [USERS.mike.id, USERS.shim.id], title: 'SEMI-FINAL 1' },
-  { teams: [USERS.pech.id, USERS.edel.id], title: 'SEMI-FINAL 2' },
-  { teams: [USERS.conner.id, USERS.slop.id], title: 'TOILET BOWL' },
+  { teams: [USERS.mike.id, USERS.conner.id], title: "DIRTY X PRESTIGE" },
+  { teams: [USERS.edel.id, USERS.slop.id], title: "CONCUSSIONS X SLOP" },
+  { teams: [USERS.shim.id, USERS.pech.id], title: "TEAM X TUANON" },
 ];
