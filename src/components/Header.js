3a9f61c16db725d337350f1e18406d2f49f47b3c
mpb9 <michaelbeebe1031@gmail.com>
2025-09-17 04:05:38 -0400

    Refactor Header component layout and styles; remove unused code and improve link styling for better consistency and readability.

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,28 +13,8 @@ export default function Header({ last_updated, active_route }) {
       ) : (
         <div className="w-1/4" />
       )}
-
-      <div className="hidden w-1/2 sm:block">
-        {active_route === "home" ? (
-          <div
-            className="text-4xl font-black text-stone-900 my-font-electro md:text-5xl"
-            style={{ cursor: active_route === "home" ? "default" : "pointer" }}
-          >
-            Order 41
-          </div>
-        ) : (
-          <NavLink
-            to="/"
-            className="text-4xl font-black transition-all duration-300 text-stone-900 my-font-electro md:text-5xl hover:text-primary hover:font-light"
-            style={{ cursor: active_route === "home" ? "default" : "pointer" }}
-          >
-            Order 41
-          </NavLink>
-        )}
-      </div>
-
-      <div className="flex items-center justify-center w-full h-full sm:justify-end sm:w-1/4">
-        {/*      <NavLink
+      <div className="flex items-center justify-center w-full h-full sm:justify-end sm:w-3/4">
+        <NavLink
           to="/"
           className={`${
             active_route === "home"
@@ -54,29 +34,11 @@ export default function Header({ last_updated, active_route }) {
         >
           MATCHUPS
         </NavLink>
-
         <a
           href={HYPERLINK.sleeper}
           target="_blank"
           rel="noreferrer"
-          className="mx-2 text-lg sm:text-xl sm:mx-3 text-stone-200 hover:text-primary hover:font-bold"
-        >
-          SLEEPER
-        </a>
-        <a
-          href={HYPERLINK.drive}
-          target="_blank"
-          rel="noreferrer"
-          className="mx-2 text-lg sm:text-xl sm:mx-3 text-stone-200 hover:text-primary hover:font-bold"
-        >
-          DRIVE
-        </a> */}
-
-        <a
-          href={HYPERLINK.sleeper}
-          target="_blank"
-          rel="noreferrer"
-          className="w-[1.9rem] h-full pt-2 hover:scale-110 transition-transform"
+          className="w-[1.75rem] h-full pt-2 hover:scale-110 transition-transform mx-3"
         >
           <div className="w-full h-full bg-no-repeat bg-contain bg-sleeper-green" />
         </a>
@@ -84,9 +46,9 @@ export default function Header({ last_updated, active_route }) {
           href={HYPERLINK.drive}
           target="_blank"
           rel="noreferrer"
-          className="w-[2.4rem] h-full ml-4 pt-[9px] sm:pt-2.5 hover:scale-110 transition-transform"
+          className="w-[2.1rem] sm:w-[2.25rem] h-full pt-[13px] sm:ml-1 sm:pt-[13px] hover:scale-110 transition-transform"
         >
-          <div className="w-full h-full bg-no-repeat bg-contain bg-drive sm:bg-drive-black" />
+          <div className="w-full h-full bg-no-repeat bg-contain bg-drive-black" />
         </a>
       </div>
     </div>
